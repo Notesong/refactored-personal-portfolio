e52ae1ce1051b5deed9b4aff34b6fa623ccdedbd
Lee Damien <notesong.git@outlook.com>
2020-03-08 23:12:44 -0400

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Header } from './components/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+      </header>
+    </div>
+  );
+}
+
+export default App;
